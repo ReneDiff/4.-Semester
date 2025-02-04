c25fe1c2225251cb50006ad01e1da2922a27b7a0
ReneDiff <au739595@gmail.com>
2025-02-04 13:00:09 +0100

    aflevering

---
+++ BAD/Assignment1/WebAPI/Models/Experience.cs
@@ -0,0 +1,10 @@
+
+namespace WebAPI.Models
+{
+    public class Experience
+    {
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public int? Price {get; set;}
+    }
+}
