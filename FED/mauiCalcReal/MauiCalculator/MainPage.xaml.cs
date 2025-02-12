07126d0c1d58484cb7239419dce6969ae1476340
ReneDiff <au739595@gmail.com>
2025-02-12 16:01:22 +0100

    Ny calc

--- FED/mauiCalcReal/MauiCalculator/MainPage.xaml.cs
+++ FED/mauiCalcReal/MauiCalculator/MainPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Maui.Controls;
 
-namespace MauiCalc;
+namespace MauiCalculator;
 
 public partial class MainPage : ContentPage
 {
--- FED/mauiCalcReal/MauiCalculator/MauiProgram.cs
+++ FED/mauiCalcReal/MauiCalculator/MauiProgram.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 
-namespace MauiCalc;
+namespace MauiCalculator;
 
 public static class MauiProgram
 {
