c313c7c13e90dbb0ba3ac9ca7fe78ae3e91bc378
ReneDiff <au739595@gmail.com>
2025-03-03 14:26:57 +0100

    BADWeek6Exerice1

---
+++ BAD/Week6/BikeStore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using BikeStore.Models;
+
+class Program
+{
+    static void Main()
+    {
+        using(var context = new BikeStoreContext())
+        {
+            var products = context.Products.ToList();
+
+            Console.WriteLine("Liste over produkter");
+            foreach (var product in products)
+            {
+                Console.WriteLine($"ID: {product.ProductId}, Navn: {product.ProductName}, Pris: {product.ListPrice}");
+            }
+        }
+    }
+}
